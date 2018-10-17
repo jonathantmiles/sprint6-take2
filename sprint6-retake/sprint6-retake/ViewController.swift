f7cc42265e12654bf83950ec4c5503ed4904408b
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-10-16 19:14:52 -0700

    starter project with lock image files

---
+++ sprint6-retake/sprint6-retake/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sprint6-retake
+//
+//  Created by Jonathan T. Miles on 10/16/18.
+//  Copyright © 2018 Jonathan T. Miles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
